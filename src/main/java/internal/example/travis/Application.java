6869d8a9d571c77aca851f6180832130ff635a9f
pluto-atom-4 <genko2@hotmail.com>
2018-09-16 14:51:26 -0700

    Rename application

--- src/main/java/internal/example/travis/Application.java
+++ src/main/java/internal/example/travis/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloTravisMavenApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloTravisMavenApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
